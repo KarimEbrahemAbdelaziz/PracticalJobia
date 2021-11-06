69c5097bbe4f7aad92f4cea5becffb35eb822897
KarimEbrahem <karimabdelazizmansour@gmail.com>
2021-11-06 16:19:45 +0200

    Add Entities to the project
    - Add User Entity

---
+++ PracticalJobia/Core/Entities/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  PracticalJobia
+//
+//  Created by Karim Ebrahem on 06/11/2021.
+//
+
+import Foundation
+
+struct User {
+    var id: String
+    var name: String
+    var email: String
+    var phoneNumber: String
+    var about: String?
+}
